115f36f86854387388b3a7f6691c69bd45868a19
Markus W. Weißmann <mww@macports.org>
2007-07-26 11:32:40 +0000

    version 0.1
    git-svn-id: https://svn.macports.org/repository/macports/users/mww@27266 d073be05-634f-4543-b044-5fe20cf6d1d6

--- select/select.sh
+++ select/select.sh
@@ -2,7 +2,8 @@
 #
 # $Id: portdestroot.tcl 27199 2007-07-24 09:09:43Z mww@macports.org $
 #
-# Copyright (c) 2007, MacPorts Project
+# Copyright (c) 2007 The MacPorts Project
+# Copyright (c) 2007 Markus W. Weissmann <mww@macports.org>
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -30,37 +31,44 @@
 # ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 
+## configuration time options
 PREFIX=__PREFIX__
-NAME=__NAME___select
-CONFPATH=${PREFIX}/etc/${NAME}
-VERSION=0.1
+NAME=__NAME__
+TOOL=${NAME}_select
+CONFPATH=${PREFIX}/etc/select/${NAME}
+VERSION=__VERSION__
+
 
 ## GLOBALS
-# dont actually execute
+# dont actually execute, just show what would have been done
 noexec=0
 # enforce action
 force=0
 # skip check for required rights
 isroot=0
+# perform "install" mode (for initial destroot)
+inst_mode=0
+DESTDIR=""
 
 
 # print the usage of this tool
 usage() {
-	echo "usage: ${NAME} [-n] [-f] [-r] [-h] [-v] version"
+	echo "usage: ${TOOL} [-n] [-f] [-r] [-h] [-v] version"
 	echo ""
-	echo "-n      Show commands to do selection but do not execute them."
-	echo "-f      Ensure the links are correct for the specified version"
-	echo "        even if it maches the current default version."
-	echo "-h      Display this help info."
-	echo "-r      Skip test for necessary rights."
-	echo "-v      Display version of ${0}."
-	echo "-l      List available options for version."
+	echo "-n        Show commands to do selection but do not execute them."
+	echo "-f        Ensure the links are correct for the specified version"
+	echo "          even if it maches the current default version."
+	echo "-h        Display this help info."
+	echo "-r        Skip test for necessary rights."
+	echo "-v        Display version of ${0}."
+	echo "-l        List available options for version."
+	echo "-i path   Install mode to perform an initial selection in path."
 	echo ""
 }
 
 # print the version of this tool
 version() {
-	echo "${NAME} v${VERSION}"
+	echo "${TOOL} v${VERSION}"
 }
 
 # list all (currently) available versions
@@ -81,10 +89,20 @@ version_is_valid() {
 
 # perform an action (command) or just display it
 action() {
-	if [ 1 == ${noexec} ]; then
-		echo "${1}"
+	if [ "rm" == ${1} ]; then
+		if [ 1 == ${noexec} ]; then
+			echo "rm -f ${2}"
+		else
+			rm -f ${2}
+		fi
+	elif [ "ln" == ${1} ]; then
+		if [ 1 == ${noexec} ]; then
+			echo "ln -sf ${2} ${3}"
+		else
+			ln -sf ${2} ${3}
+		fi
 	else
-		${1}
+		return 1
 	fi
 }
 
@@ -94,15 +112,21 @@ select_version() {
 	local error=0
 	local i=1
 	local empty=0
+	echo "Selecting version \"${1}\" for ${NAME}"
 	for target in $(cat ${CONFPATH}/base); do
 		src=$(head -n ${i} ${CONFPATH}/${1} | tail -n 1)
 
 		empty=0
 		# test if line starts with '-' -> dont link, just rm original
 		if [ "-" == $(echo ${src} | colrm 2) ]; then
-			action "rm -f ${PREFIX}${target}"
+			# source is unavailable for this file
+			action "rm" "${DESTDIR}${PREFIX}/${target}"
+		elif [ "/" == $(echo ${src} | colrm 2) ]; then
+			# source has an absolute path
+			action "ln" "${src}" "${DESTDIR}${PREFIX}/${target}"
 		else
-			action "ln -sf \"${PREFIX}${src}\" \"${PREFIX}${target}\""
+			# source has relative path
+			action "ln" "${PREFIX}/${src}" "${DESTDIR}${PREFIX}/${target}"
 		fi
 		let "error = error + ${?}"
 		let "i = i + 1"
@@ -117,7 +141,7 @@ if [ ${#} == 0 ]; then
 fi
 
 # parse command line args
-args=$(/usr/bin/getopt fhnlrv $*)
+args=$(/usr/bin/getopt i:fhnlrv $*)
 set -- ${args}
 for i; do
 	case "${i}" in
@@ -128,20 +152,25 @@ for i; do
 		-f)
 			force=1; shift;;
 		-l)
-			list; exit 0;;
+			list_version; exit 0;;
 		-r)
 			isroot=1; shift;;
 		-v)
 			version; exit 0;;
+		-i)
+			inst_mode=1; DESTDIR=${2}; shift; shift;;
 		--)
 			shift; break;;
 	esac
 done
 
-#echo "isroot: ${isroot}"
-#echo "noexec: ${noexec}"
-#echo "force: ${force}"
-#echo "arg: ${1}"
+# install mode - bypass all checks and add DESTDIR
+if [ "1" = "${inst_mode}" ]; then
+	echo "install mode: destroot: \"${DESTDIR}\""
+	CONFPATH=${DESTDIR}${CONFPATH}
+	select_version ${1}
+	exit ${?}
+fi
 
 # test if chosen version is available
 version_is_valid $1
